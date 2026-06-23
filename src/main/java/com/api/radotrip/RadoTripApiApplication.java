457cb863ef9a1ff3392fb904645479b8f8ffa63a
LHJ <hejeoung@nsoftware.kr>
2026-06-23 14:52:54 +0900

    init setting

---
+++ src/main/java/com/api/radotrip/RadoTripApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.radotrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadoTripApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RadoTripApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/radotrip/RadoTripApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.radotrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadoTripApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
