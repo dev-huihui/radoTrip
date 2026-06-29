e85408a877f5a7d1f9b50f1b06450da7279572c2
dev-huihui <hejeoung@nsoftware.kr>
2026-06-29 10:19:34 +0900

    build: yaml 파일 수정 및 서버 기동을 위한 세팅

--- src/main/java/com/api/radotrip/RadoTripApiApplication.java
+++ src/main/java/com/api/radotrip/RadoTripApiApplication.java
@@ -2,10 +2,12 @@ package com.api.radotrip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication // Spring Boot 자동 설정
 @EnableScheduling // 스케줄링 활성화
+@MapperScan("com.api.radotrip.mapper.*") // Mapper 패키지 지정
 public class RadoTripApiApplication {
 
     public static void main(String[] args) {
